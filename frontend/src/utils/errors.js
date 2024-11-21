16454de4e4a99cb7268e83c31befbb7b7ce3daa8
Adithya Prasad <adithyasridharprasad@gmail.com>
2024-11-21 12:32:58 +0530

    Rename existing frontend to frontend-v1

--- frontend/src/utils/errors.js
+++
@@ -1,9 +0,0 @@
-import { toast } from "react-toastify";
-
-const handleErrors = (error, defaultMessage = "Internal Server Error") => {
-  if (import.meta.env.DEV) console.log(error);
-  const errorMessage = error.response?.data?.message || defaultMessage;
-  toast.error(errorMessage);
-};
-
-export default handleErrors;
